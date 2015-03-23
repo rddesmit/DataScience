8ea785f95291f0672810a6706c5783d4a41b02ec
Rudie de Smit <rddesmit@hotmail.com>
2015-03-23 21:08:00 +0100

    Added some tests.

--- src/test/scala/UserPreference$Test.scala
+++ src/test/scala/UserPreference$Test.scala
@@ -1,3 +1,4 @@
+import Strategys.UserItemStrategys.RatingComparison
 import User.UserPreference
 import org.scalatest.{FlatSpec, Matchers}
 
@@ -6,7 +7,7 @@ import org.scalatest.{FlatSpec, Matchers}
  */
 class UserPreference$Test extends FlatSpec with Matchers {
 
-  "average" should "return average" in {
+  "Average" should "return average" in {
     val userOne = UserPreference("Amy").addRating("a", 3.5).addRating("b", 3.0).addRating("c", 4.0)
     val userTwo = UserPreference("Bill").addRating("a", 2.0).addRating("b", 5.0).addRating("c", 2.0)
     val userThree = UserPreference("Clara").addRating("a", 3).addRating("c", 5.0)
@@ -14,5 +15,26 @@ class UserPreference$Test extends FlatSpec with Matchers {
     userOne.averageRating should be(3.5)
     userTwo.averageRating should be(3.0)
     userThree.averageRating should be(4.0)
+  }
+
+  "Has different ratings" should "return TRUE" in {
+    val userOne = UserPreference("Amy").addRating("a", 3.5).addRating("b", 3.0).addRating("c", 4.0)
+    val userTwo = UserPreference("Clara").addRating("a", 3).addRating("c", 5.0)
+
+    userOne.hasDiffRatings(userTwo) should be(true)
+  }
+
+  "Has different ratings" should "return FALSE" in {
+    val userOne = UserPreference("Amy").addRating("a", 3.5).addRating("b", 3.0).addRating("c", 4.0)
+    val userTwo = UserPreference("Bill").addRating("a", 2.0).addRating("b", 5.0).addRating("c", 2.0)
+
+    userOne.hasDiffRatings(userTwo) should be(false)
+  }
+
+  "To rating comparisons" should "return a ordered list" in {
+    val userOne = UserPreference("Amy").addRating("a", 3.5).addRating("b", 3.0).addRating("c", 4.0)
+    val userTwo = UserPreference("Bill").addRating("c", 2.0).addRating("a", 2.0).addRating("b", 5.0)
+
+    userOne.toRatingComparisons(userTwo) should be(List(RatingComparison(3.5, 2.0), RatingComparison(3.0, 5.0), RatingComparison(4.0, 2.0)))
   }
 }
